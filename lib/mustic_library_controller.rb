01b6b92fcd94578c9be1c8e466e235d03498c9fe
Josh Ellis <joshe523@gmail.com>
2020-03-06 01:55:52 +0000

    Automatically backed up by Learn

--- lib/mustic_library_controller.rb
+++ lib/mustic_library_controller.rb
@@ -70,6 +70,8 @@ class MusicLibraryController
         list_songs_by_artist
       when "list genre"
         list_songs_by_genre
+      when "play song"
+        list_songs_by_genre
       end
     end
   end
