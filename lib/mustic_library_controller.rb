c8d3fd5a80ea2e2a0ff3553b0cec48d97f0577c4
Josh Ellis <joshe523@gmail.com>
2020-03-06 01:16:55 +0000

    Automatically backed up by Learn

--- lib/mustic_library_controller.rb
+++ lib/mustic_library_controller.rb
@@ -2,8 +2,8 @@ class MusicLibraryController
   attr_accessor
 
   def initialize(path="./db/mp3s")
-    importer = MusicImporter.new(path)
-    importer.import
+    @importer = MusicImporter.new(path)
+    @importer.import
   end
 
   def call
