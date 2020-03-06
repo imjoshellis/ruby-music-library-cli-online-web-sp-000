13c02f8895e8586adb5b51503f04b9facc594433
Josh Ellis <joshe523@gmail.com>
2020-03-06 01:48:54 +0000

    Automatically backed up by Learn

--- lib/mustic_library_controller.rb
+++ lib/mustic_library_controller.rb
@@ -37,6 +37,14 @@ class MusicLibraryController
     .each_with_index{|song, idx| puts "#{(idx+1).to_s}. #{song.artist.name} - #{song.name}"} if genre
   end
 
+  def play_song
+    puts "Which song number would you like to play?"
+    list_songs
+    response = gets.chomp
+    Song.all.sort_by{|song| song.name}
+      .each_with_index{|song, idx| puts "Playing #{song.name} by #{song.artist.name}" if idx == response}
+  end
+
   def call
     response = ""
     while response != "exit"
