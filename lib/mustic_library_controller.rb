32c5c0bccc53d41617223012287fc4f18b4da6b0
Josh Ellis <joshe523@gmail.com>
2020-03-06 01:48:57 +0000

    Automatically backed up by Learn

--- lib/mustic_library_controller.rb
+++ lib/mustic_library_controller.rb
@@ -39,7 +39,7 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    list_songs
+    # list_songs
     response = gets.chomp
     Song.all.sort_by{|song| song.name}
       .each_with_index{|song, idx| puts "Playing #{song.name} by #{song.artist.name}" if idx == response}
